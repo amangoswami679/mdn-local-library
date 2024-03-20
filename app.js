717b80c4fe7d71e6360b24c7a0c5dc4742af124f
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-03-20 21:26:21 +0530

    Add: default connection to the database

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = "mongodb+srv://amangoswami679:gtavc55154@cluster0.nxyxqni.mongodb.net/mdn_local_library?retryWrites=true&w=majority&appName=Cluster0";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,12 +32,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
