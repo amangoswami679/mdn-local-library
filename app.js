fa7ade25a0c6c62c3c0ef3edd498ca741ef9eb2c
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-03-23 12:00:13 +0530

    Add: Catalog module and router to middleware stack

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); // Import routes for "catalog" area of site
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
