200fccd6ad11184e608c1fb905dc9dea403def82
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-03-23 17:55:32 +0530

    Add: due_date_formatted virtual using luxon

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const { DateTime } = require('luxon');
+
 const BookInstanceSchema = new Schema({
     book: { type: Schema.Types.ObjectId, ref: "Book", required: true }, // reference to the associated book
     imprint: { type: String, required: true },
@@ -18,6 +20,10 @@ const BookInstanceSchema = new Schema({
 BookInstanceSchema.virtual("url").get(function () {
     // We don't use an arrow function as we'll need the this object
     return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
