c499764bddefc319a1765ae1b2476e43dd394fe7
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-03-23 12:03:51 +0530

    Fix: Wrong controller method for create book route

--- routes/catalog.js
+++ routes/catalog.js
@@ -12,7 +12,7 @@ const book_instance_controller = require('../controllers/bookinstanceController'
 router.get('/', book_controller.index);
 
 // GET request for creating a Book. NOTE This must come before routes that display Book (uses id). (https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes#route_parameters Note section)
-router.get('/book/create', book_controller.book_delete_get);
+router.get('/book/create', book_controller.book_create_get);
 
 // POST request for creating Book.
 router.post('/book/create', book_controller.book_create_post);
