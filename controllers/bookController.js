6e0a9d5b7223155942a1eae20df4ef0af2fefcb8
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-04-01 20:46:57 +0530

    Update: Book deletion route handling

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -160,12 +160,29 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete GET");
+  // Get details of book and all instances (in parallel)
+  const [book, allBookInstances] = await Promise.all([
+    Book.findById(req.params.id).exec(),
+    BookInstance.find({ book: req.params.id }, "imprint").exec(),
+  ]);
+
+  if (book === null) {
+    // No results.
+    res.redirect("/catalog/books");
+  }
+
+  res.render("book_delete", {
+    title: "Delete Book",
+    book: book,
+    book_instances: allBookInstances,
+  });
 });
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
+  await Book.findByIdAndDelete(req.body.bookid);
+
+  res.redirect("/catalog/books");
 });
 
 // Display book update form on GET.
