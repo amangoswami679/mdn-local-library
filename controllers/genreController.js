5a42e34616bb5c6f4251571da1d12c4729054e73
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-04-01 21:11:05 +0530

    Update: Genre deletion route handling

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -92,12 +92,28 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  // Get details of genre and all associated books (in parallel)
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (genre == null) {
+    res.redirect("/catalog/genres");
+  }
+
+  res.render("genre_delete", {
+    title: "Delete Genre",
+    genre: genre,
+    genre_books: booksInGenre,
+  });
 });
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  await Genre.findByIdAndDelete(req.body.genreid);
+
+  res.redirect("/catalog/genres");
 });
 
 // Display Genre update form on GET.
