fe3d60d358a6783420bb5a00d366be232ad8b739
Aman Goswami <41533392+amangoswami679@users.noreply.github.com>
2024-04-01 21:01:06 +0530

    Update: Book instance deletion route handling

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -100,12 +100,24 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  const bookinstance = await BookInstance.findById(req.params.id, "imprint status").exec();
+
+  if (bookinstance === null) {
+    // No results.
+    res.redirect("/catalog/bookinstances");
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete Book Instance",
+    bookinstance: bookinstance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  await BookInstance.findByIdAndDelete(req.body.bookinstanceid);
+
+  res.redirect("/catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
